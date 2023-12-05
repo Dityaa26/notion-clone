db4d25f79707896ecf9d73c208f7049fab8885ae
Dityaa26 <aditya261862@gmail.com>
2023-12-05 10:59:12 +0530

    Marketing page done

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
+import Heading from './_components/heading'
+import Heroes from './_components/heroes'
+import Footer from './_components/footer'
 
-export default function Home() {
-  return ( 
-    <div>
-      <Button variant={'purple'}>Click Me</Button>
+export default function MarketingPage() {
+  return (
+    <div className="min-h-full dark:bg-[#1f1f1f] flex flex-col">
+      <div className="flex flex-col justify-center items-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
